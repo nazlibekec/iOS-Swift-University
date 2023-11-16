93c879f79c1da37fad4db965adf6c2751e437bf2
nazlibekec <nazlibekec@gmail.com>
2023-11-16 11:01:01 +0300

    Nostoryboard ayarları yapıldı.

---
+++ University/University/Controller/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  University
+//
+//  Created by Nazlı Bekeç on 16.11.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    // MARK: -LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+    
+}
+
+//MARK: - Helpers
+
